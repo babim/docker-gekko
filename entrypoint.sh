75d6dbf846f7aae83f776c77fef7eaff7862aa5f
Duc Anh Babim <babim@matmagoc.com>
2017-11-22 22:35:24 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 if [ -z "`ls /usr/src/app`" ]; then 
 cp -R /start/gekko/* /usr/src/app
+fi
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /usr/src/app/web/vue/UIconfig.js
 sed -i 's/localhost/'${HOST}'/g' /usr/src/app/web/vue/UIconfig.js
