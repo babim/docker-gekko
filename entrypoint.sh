ac0dbd8da6afefe45cca81088b1deffd42a30b6e
Duc Anh Babim <babim@matmagoc.com>
2017-11-22 23:12:48 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ if [ -z "`ls /usr/src/app`" ]; then
 cp -R /start/gekko/* /usr/src/app
 fi
 
+# ssh
+if [ -f "/runssh.sh" ]; then /runssh.sh; fi
+
 sed -i 's/127.0.0.1/0.0.0.0/g' /usr/src/app/web/vue/UIconfig.js
 sed -i 's/localhost/'${HOST}'/g' /usr/src/app/web/vue/UIconfig.js
 sed -i 's/3000/'${PORT}'/g' /usr/src/app/web/vue/UIconfig.js
