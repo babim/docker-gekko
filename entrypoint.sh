d33e9dcfc32f3bb041f401f70003c5583a130238
Duc Anh Babim <babim@matmagoc.com>
2017-11-22 21:10:59 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "`ls /usr/src/app`" ]; then 
+cp -R /start/gekko/* /usr/src/app
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /usr/src/app/web/vue/UIconfig.js
+sed -i 's/localhost/'${HOST}'/g' /usr/src/app/web/vue/UIconfig.js
+sed -i 's/3000/'${PORT}'/g' /usr/src/app/web/vue/UIconfig.js
+exec "$@"
