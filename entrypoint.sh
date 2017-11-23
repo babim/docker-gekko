36010114799426a86292f7eaff0c9ed37b81b2ee
Duc Anh Babim <babim@matmagoc.com>
2017-11-23 07:48:15 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ -z "`ls /usr/src/app`" ]; then 
-cp -R /start/gekko/* /usr/src/app
-fi
-
 sed -i 's/127.0.0.1/0.0.0.0/g' /usr/src/app/web/vue/UIconfig.js
 sed -i 's/localhost/'${HOST}'/g' /usr/src/app/web/vue/UIconfig.js
 sed -i 's/3000/'${PORT}'/g' /usr/src/app/web/vue/UIconfig.js
