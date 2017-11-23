3040cb6d6aeebf3b51d3ff51e8543f7251356039
Duc Anh Babim <babim@matmagoc.com>
2017-11-23 10:03:09 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,9 @@
 sed -i 's/127.0.0.1/0.0.0.0/g' /usr/src/app/web/vue/UIconfig.js
 sed -i 's/localhost/'${HOST}'/g' /usr/src/app/web/vue/UIconfig.js
 sed -i 's/3000/'${PORT}'/g' /usr/src/app/web/vue/UIconfig.js
+
+if [[ -z "${DRIVER}" ]]; then
+sed -i 's/sqlite/'${DRIVER}'/g' /usr/src/app/web/vue/UIconfig.js
+fi
+
 exec "$@"
